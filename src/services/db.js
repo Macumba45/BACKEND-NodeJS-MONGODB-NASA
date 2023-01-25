2e0a98f8925f1503a2cd7e5b3582c30a8cc5d706
Macumba45 <gonzalolovo@gmail.com>
2023-01-25 12:28:01 +0100

    env files

--- src/services/db.js
+++ src/services/db.js
@@ -4,7 +4,10 @@ mongoose.set("strictQuery", false);
 
 export const connectToDb = async () => {
 
-    await mongoose.connect('mongodb://localhost:27017/nasa_dbd', {})
+    // await mongoose.connect('mongodb://localhost:27017/nasa_dbd', {})
+    // console.log("Connected to MongoDB");
+
+    await mongoose.connect(`${process.env.MONGODB}://${process.env.HOST}:${process.env.PORTHOST}/${process.env.DBNAME}`, {})
     console.log("Connected to MongoDB");
 
 }
